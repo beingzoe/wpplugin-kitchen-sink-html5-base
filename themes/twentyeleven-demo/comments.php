157f6b16bd4746e031e9f32761fcf41fdb363d43
zoe somebody <me@beingzoe.com>
2011-02-02 17:12:45 -0800

    Put back logic to recreate the logic for using aria-required="true" on comment form. Woops.

--- themes/twentyeleven-demo/comments.php
+++ themes/twentyeleven-demo/comments.php
@@ -72,13 +72,17 @@ if ( post_password_required() ) {
 } // End have_comments()
 
 // Comment form arguments - Altered markup to allow for more customization - ideally <p> would be replaced with a more semantically correct element
+// See comment-template.php comment_form()
+$req = get_option( 'require_name_email' );
+$aria_req = ( $req ? " aria-required='true'" : '' );
+echo "req = $req   aria_req = $aria_req";
 $comment_form_fields =  array(
 	'author' => '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
-	            '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"'  . ' /></p>',
+	            '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
 	'email'  => '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . ( $req ? '<span class="required">*</span>' : '' ) . '</label> ' .
 	            '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
 	'url'    => '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
-	            '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30"' . $aria_req . ' /></p>',
+	            '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>',
 );
 
 /**
