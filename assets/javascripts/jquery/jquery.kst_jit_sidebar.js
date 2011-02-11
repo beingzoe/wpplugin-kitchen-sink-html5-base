eb4faf64bb1ede3d34b1e54d90a0c69c44c2de69
zoe somebody <me@beingzoe.com>
2011-02-10 18:33:50 -0800

    Started the process of figuring out how to reasonably stop the jit sidebar from "floating" when it reaches the footer. I shouldn't commit this but oh well.

--- assets/javascripts/jquery/jquery.kst_jit_sidebar.js
+++ assets/javascripts/jquery/jquery.kst_jit_sidebar.js
@@ -13,29 +13,44 @@
 
 $.fn.jit_sidebar = function(options) {
 
-    jit = $(".widget_jit_sidebar");
+    // Set defaults
+    var params = {
+      'trigger'         : '.widget_jit_sidebar',
+      'footer'         : '#ft'
+    };
 
-    $(".widget_jit_sidebar ~ .widget").wrapAll("<div class='jit_sidebar_container'></div>");
+    return this.each(function() { // Allow chaining
 
-    viewportHeight = window.innerHeight ? window.innerHeight : $(window).height(); // With Opera 9.5, $(window).height() returns the document height
-
-    $(window).scroll(function(){
-
-        /* protect from viewportHeight being shorter than the jit and funking things up (and making bottom-most widgets unusable */
-        if  ( $(".jit_sidebar_container").height() < viewportHeight && $(window).scrollTop() > jit.offset().top ){
-            $(".jit_sidebar_container").css({
-                position: "fixed",
-                top: "0px",
-                width: $(".jit_sidebar_container").width() /* in case the width wasn't explicit - breaks fluid layout though */
-              });
+        // Merge options
+        if ( options ) {
+            params = $.extend( params, options );
         }
-        if  ($(window).scrollTop() <= jit.offset().top){
-            $(".jit_sidebar_container").css({
-                position: "relative",
-                top: jit.offset
-              });
-        }
+
+        jit = $(params.trigger)//   $(".widget_jit_sidebar");
+        ft_height = $(params.footer).height();
+
+        $(".widget_jit_sidebar ~ .widget").wrapAll("<div class='jit_sidebar_container'></div>");
+
+        viewportHeight = window.innerHeight ? window.innerHeight : $(window).height(); // With Opera 9.5, $(window).height() returns the document height
+
+        $(window).scroll(function(){
+
+            /* protect from viewportHeight being shorter than the jit and funking things up (and making bottom-most widgets unusable */
+            if  ( $(".jit_sidebar_container").height() < viewportHeight && ($(window).scrollTop()) > jit.offset().top ){
+                $(".jit_sidebar_container").css({
+                    position: "fixed",
+                    top: "0px",
+                    width: $(".jit_sidebar_container").width() /* in case the width wasn't explicit - breaks fluid layout though */
+                  });
+            }
+            if  ($(window).scrollTop() <= (jit.offset().top)){
+                $(".jit_sidebar_container").css({
+                    position: "relative",
+                    top: jit.offset
+                  });
+            }
+        });
     });
-};
+}
 
 })(jQuery);
